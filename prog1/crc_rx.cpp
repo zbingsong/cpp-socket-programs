d98e5b48b8db250540c3b64bd543d1ac592a5224
Bingsong Zeng <bingsong@usc.edu>
2023-02-03 11:51:10 -0800

    added comments

--- prog1/crc_rx.cpp
+++ prog1/crc_rx.cpp
@@ -5,15 +5,18 @@
 
 int main()
 {
+  // read the data file
   std::string filename = "dataRx.txt";
   std::ifstream inFile(filename.c_str());
+  // create a new crc encoder
   CrcEncoder* encoder = new CrcEncoder();
+  // store file content on each line in this variable
   std::string line;
-  std::string result;
+  // read file line by line
   while (std::getline(inFile, line)) {
     const char* codeword = line.c_str();
-    result = encoder->verifyCodeword(codeword) ? "Pass" : "Not Pass";
-    std::cout << result << std::endl;
-    //delete[] codeword;
+    // output verification result
+    // encoder->verifyCodeword() returns a boolean
+    std::cout << (encoder->verifyCodeword(codeword) ? "Pass" : "Not Pass") << std::endl;
   }
 }
